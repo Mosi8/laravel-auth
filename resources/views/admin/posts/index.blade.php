984fdd87b75a18040b398418dbfeceb6f38fb32b
Mosi8 <mauriziomosi8@gmail.com>
2022-03-15 22:29:56 +0100

    completo con css da fare e sistemare posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,24 @@
+{{-- @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            Ciao
+            @dump($posts)
+        </div>
+    </div>
+</div>
+@endsection --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @dump($posts)
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function (){
+        Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('/posts', 'PostController'); 
+    });
+
